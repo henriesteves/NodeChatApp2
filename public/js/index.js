073c68fda68bb90acc14b1f2584284fd607afbab
Henrique Esteves <henriesteves@gmail.com>
2018-10-13 21:53:20 -0300

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -11,15 +11,15 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
 
-  const li = jQuery('<li></li>');
+  var li = jQuery('<li></li>');
   li.text(`${message.from}: ${message.text}`);
 
   jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
-  const li = jQuery('<li></li>');
-  const a = jQuery('<a target="_blank">My Current Location</a>');
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My Current Location</a>');
 
   li.text(`${message.from}: `);
   a.attr('href', message.url)
@@ -38,29 +38,36 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-    
+    messageTextbox.val('');
   });
 });
 
-const locationButton = jQuery('#send-location');
+var locationButton = jQuery('#send-location');
 
 locationButton.on('click', function () {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
     // console.log(position);
+
+    locationButton.removeAttr('disabled').text('Send location');
 
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.');
   });
 });
